80fd445062a3282ea41d5b1f2a7cac9c36fe618d
Jang JaeHyeok <hyeok9942@gmail.com>
2023-08-01 17:38:01 +0900

    0801 labtop

--- codec.py
+++ codec.py
@@ -1,40 +1,79 @@
 from enum import Enum
 import traceback
-from typing import Callable
+from typing import Callable, Any
 
 
 class Codec:
+    # priavate field -----
+    __ENCODE = 0        # function table mode
+    __DECODE = 1        # function table mode
+    __EDP = b'EDP'      # Encoding Data Point: 인코딩 데이터 시작점을 알리기 위한 더미 데이터
+
+    # public static method -----
     @staticmethod
-    def encode(param) -> list[bytes] | bool:
+    def encode(param) -> list[bytes] | None:
         try:
-            return Codec.__getFunctionTable()[type(param).__name__](param)
+            # [b'EDP', b'{type}', b'{data}']의 형태로 반환
+            return [Codec.__EDP] + Codec.__getFunctionTable(Codec.__ENCODE)[type(param).__name__](param)
         except KeyError:
+            # int, bool, string, list외에 type이 들어왔을 경우
             traceback.print_exc()
             print(f"EncodingError: '{type(param).__name__}' 형식은 부호화를 지원하지 않습니다.")
+            return None
     
 
     @staticmethod
-    def decode(param: list[bytes]) -> list[str]:
-         return [part.decode() for part in param]
+    def decode(param: list[bytes]) -> tuple:
+        encoded = []                                # 인코딩된 데이터
+        retValue = []                               # 반환값
+        
+        # EDP 탐색(EDP 이후 부터 인코딩된 데이터)
+        for i in range(len(param)):
+            if param[i] == Codec.__EDP:
+                encoded = param[i + 1:]
+                param = param[:i]
+                break
+        
+        # 디코딩 진행
+        for i in range(0, len(encoded) - 1, 2):
+            try:
+                retValue.append(Codec.__getFunctionTable(Codec.__DECODE)[encoded[i].decode()](encoded[i + 1]))
+            # 디코딩을 지원하지 않는 타입(int, bool, string 지원)
+            except KeyError:
+                traceback.print_exc()
+                print(f"EncodingError: '{type(encoded[i]).decode()}' 형식은 부호화를 지원하지 않습니다.")
+
+        return tuple(param + retValue)
     
 
-    # private method
+    # private method -----
+    # type에 따른 인/디코딩 방식을 다르게 적용하기 위해, 각 기능을 제공하는 함수 테이블 반환(lookup table)
     @staticmethod
-    def __getFunctionTable() -> (
-        Callable[[int], list[bytes]] | 
-        Callable[[list[bytes]]]
+    def __getFunctionTable(mode: int) -> (
+        Callable[[int], list[bytes]] | Callable[[list[bytes]], Any]
     ):
-        return {
-            int.__name__        :       Codec.__encodeInt,
-            bool.__name__       :       Codec.__encodeBool,
-            str.__name__        :       Codec.__encodeString,
-            list.__name__       :       Codec.__encodeList,
-        }
+        table = [
+            # index = 0: Encode Function table
+            {
+                int.__name__        :       Codec.__encodeInt,
+                bool.__name__       :       Codec.__encodeBool,
+                str.__name__        :       Codec.__encodeString,
+                list.__name__       :       Codec.__encodeList,
+            },
+            # index = 1: Decode Function table
+            {
+                int.__name__        :       Codec.__decodeInt,
+                bool.__name__       :       Codec.__decodeBool,
+                str.__name__        :       Codec.__decodeString,
+            }
+        ]
 
-    ## encode
+        return table[mode]
+
+    # 인코딩 함수: [b'{name of type}, b'{encoded data}'] 형태로 반환
     @staticmethod
     def __encodeInt(param: int) -> list[bytes]:
-        return [type(param).__name__.encode(), param.to_bytes()]      # [b'자료형', b'값']의 형태로 반환
+        return [type(param).__name__.encode(), param.to_bytes()]
     
     @staticmethod
     def __encodeBool(param: bool) -> list[bytes]:
@@ -48,19 +87,29 @@ class Codec:
     def __encodeList(param: list) -> list[bytes]:
         retValue = []
         for item in param:
-            retValue.extend(Codec.__getFunctionTable()[type(item).__name__](item))
+            retValue.extend(Codec.__getFunctionTable(Codec.__ENCODE)[type(item).__name__](item))
         
         return retValue
     
-    ## decode
+    # 디코딩 함수
     @staticmethod
-    def __decodeInt(param: )
-         
-
+    def __decodeInt(param: bytes) -> int | None:
+        try:
+            return int.from_bytes(param)
+        except TypeError:
+            print(f"TypeError: '{param}' is not int type")
+            return None
+    
     @staticmethod
-    def test(param: list) -> list[bytes]:
-        Codec.__encodeList(param)
-    
-
-    
-
+    def __decodeBool(param: bytes) -> bool | None:
+        if param.decode() == str(True):
+            return True
+        elif param.decode() == str(False):
+            return False
+        else:
+            print(f"TypeError: '{param.decode()}' is not boolean type")
+            return None
+        
+    @staticmethod
+    def __decodeString(param: bytes) -> str:
+        return param.decode()
--- test.py
+++ test.py
@@ -43,9 +43,54 @@ def dealer():
 
 
 
-print(Codec.encode(10))
-print(Codec.encode(False))
-print(Codec.encode('hello'))
-print(Codec.encode([10, 20, 30]))
-print(Codec.encode([10, '20', True]))
-print(Codec.encode(1.3))
+# print(Codec.encode(10))
+# print(Codec.encode(False))
+# print(Codec.encode('hello'))
+# print(Codec.encode([10, 20, 30]))
+# print(Codec.encode([10, '20', True]))
+# print(Codec.encode(1.3))
+
+# def func(param: bytes):
+#     return param.decode()
+
+# datas = [b'jang', b'str', b'hello world']
+# encodingDatas = []
+
+# start = 0
+# for i in range(len(datas)):
+#     if 'str' == datas[i].decode():
+#         encodingDatas = datas[i:]
+#         datas = datas[:i]
+#         break
+
+# print(datas)
+# print(encodingDatas)
+
+# table = {
+#     str.__name__        :       func
+# }
+
+# for i in range(0, len(encodingDatas), 2):
+#     print(table[encodingDatas[i].decode()](encodingDatas[i + 1]))
+
+
+# temp = [1, 2, 3, 4, 5, 6]
+
+# for i in range(0, len(temp) - 1, 2):
+#     print(temp[i + 1])
+
+data = [10, 20, 'hello World', False]
+
+encoding = Codec.encode(data)
+print(f'encoding: {encoding}')
+
+decoding = Codec.decode(encoding)
+print(f'decoding: {decoding}')
+
+for item in decoding:
+    print(f"type of '{item}': {type(item).__name__}")
+
+e = Codec.encode(10)
+d = Codec.decode(e)
+print(e)
+print(d)
